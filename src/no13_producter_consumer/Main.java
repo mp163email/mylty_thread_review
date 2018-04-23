49b348455192917eeda12d739bcc52c9e95545e7
mp163email <574474931@qq.com>
2018-04-23 18:23:47 +0800

    修改了一些注释

--- src/no13_producter_consumer/Main.java
+++ src/no13_producter_consumer/Main.java
@@ -3,7 +3,7 @@ package no13_producter_consumer;
 /**
  * 使用阻塞队列（BlockingQueue）方式实现生产消费
  * 与使用synchronized + wait-notify相比, 阻塞队列的方式更为简洁
- * 把synchronized和wait-notify操作封装到了阻塞队列里面.是程序员使用起来更方便
+ * 把synchronized和wait-notify操作封装到了阻塞队列里面.使程序员使用起来更方便
  * Auth: miaopeng
  * Date: 2018-04-23 11:04:43
  */
